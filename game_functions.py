d5742fe15703f77c394323ef4e89d28055bdfdeb
aa1771exeter <aa1771@exeter.ac.uk>
2026-01-20 12:04:59 +0000

    game 3 completion

--- game_functions.py
+++ game_functions.py
@@ -9,7 +9,7 @@ def pick_value(poss_values):
 def check_higher_lower(current_val, next_val, user_input):
     if (user_input == "h") and (next_val > current_val):
         return True
-        
+
     elif (user_input == "l") and (current_val > next_val):
        
         return True 
@@ -19,4 +19,19 @@ def check_higher_lower(current_val, next_val, user_input):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    
+   
+    for index, char in enumerate(word):
+        if char == letter:
+            board[index] = letter
+        
+        
+
+
+    if letter in word:
+        print("Well done!", letter, " is in the word")
+        return True
+    else:
+        print("Sorry", letter, " is not in the word")
+        return False
+
